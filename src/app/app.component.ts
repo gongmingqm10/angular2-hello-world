c3c03686f2e5be598d189542e04abc34782c8ed6
mingong <gongmingqm10@foxmail.com>
2017-09-23 22:26:44 +0800

    Delete the components in Chapter2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Article} from './article/article';
 
 @Component({
   selector: 'app-root',
@@ -8,24 +7,8 @@ import {Article} from './article/article';
 })
 export class AppComponent implements OnInit {
 
-  articles: Article[];
-
-  constructor() {
-    this.articles = [
-      new Article('Angular 2', 'http://angular.io', 3),
-      new Article('Android', 'https://android.com', 10),
-      new Article('GraphQL', 'https://graphql.com')
-    ];
-  }
-
   ngOnInit(): void {
     console.log('App init successful');
   }
-
-  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    this.articles.push(new Article(title.value, link.value));
-    return false;
-  }
-
 
 }
